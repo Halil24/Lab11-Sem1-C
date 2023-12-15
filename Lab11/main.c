f45206555de5d9dda0e8dbac7e6769c67e235cd5
Halil Bagosi <halilbagosi24@icloud.com>
2023-12-15 08:46:00 +0100

    Initial Commit

---
+++ Lab11/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab11
+//
+//  Created by Halil Bagosi on 15.12.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
